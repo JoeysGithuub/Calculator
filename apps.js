88d82eedb8506604c6e03fce93b1c1b7b6c484d0
Joey <smithjoey5123@gmail.com>
2018-05-24 15:32:47 -0500

    more

--- apps.js
+++ apps.js
@@ -2,27 +2,27 @@
 document.getElementById("add").addEventListener("click", function(){
     let numb1 = parseInt(document.getElementById("numb1").value);
     let numb2 = parseInt(document.getElementById("numb2").value);
-    let value = numb1 + numb2;
-    document.querySelector("#total").innerHTML = `The result of ${numb1} plus ${numb2} is ${total}`
+    let total = numb1 + numb2;
+    document.querySelector("#total").innerHTML = `The answer to ${numb1} plus ${numb2} is ${total}`
 })
 
-document.getElementById("subtract").addEventListener("click", function(){
+document.getElementById("sub").addEventListener("click", function(){
     let numb1 = parseInt(document.getElementById("numb1").value);
     let numb2 = parseInt(document.getElementById("numb2").value);
-    let value = numb1 - numb2;
-    document.querySelector("#total").innerHTML = `The result of ${numb1} minus ${numb2} is ${total}`
+    let total = numb1 - numb2;
+    document.querySelector("#total").innerHTML = `The answer to ${numb1} minus ${numb2} is ${total}`
 })
 
-document.getElementById("divide").addEventListener("click", function(){
-    let numb1 = parseInt(document.getElementById("numb1").total);
-    let numb2 = parseInt(document.getElementById("numb2").total);
-    let value = numb1 / numb2;
-    document.querySelector("#total").innerHTML = `The result of ${numb1} divided by ${numb2} is ${total}`
-})
-
-document.getElementById("multiply").addEventListener("click", function(){
-    let numb1 = parseInt(document.getElementById("numb1").total);
+document.getElementById("mult").addEventListener("click", function(){
+    let numb1 = parseInt(document.getElementById("numb1").value);
     let numb2 = parseInt(document.getElementById("numb2").value);
-    let value = numb1 * numb2;
-    document.querySelector("#total").innerHTML = `The result of ${numb1} multiplied by ${numb2} is ${total}`
+    let total = numb1 * numb2;
+    document.querySelector("#total").innerHTML = `The answer to ${numb1} multiplied by ${numb2} is ${total}`
+})
+
+document.getElementById("div").addEventListener("click", function(){
+    let numb1 = parseInt(document.getElementById("numb1").value);
+    let numb2 = parseInt(document.getElementById("numb2").value);
+    let total = numb1 / numb2;
+    document.querySelector("#total").innerHTML = `The answer to ${numb1} divided by ${numb2} is ${total}`
 })
